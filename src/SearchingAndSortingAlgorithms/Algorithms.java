0180a7cf4705b32cde855f7c8f9c835c0fa6a971
League <League@administorsimac.attlocal.net>
2017-11-01 18:56:36 -0700

    Algorithms (IP), SearchingAndSortingAlgorithms (IP)

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -2,6 +2,7 @@ package SearchingAndSortingAlgorithms;
 
 import java.util.List;
 
+/* 1. Create a method that will find return the first index of a broken egg */
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
 		int a;
@@ -16,7 +17,7 @@ public class Algorithms {
 	}
 
 	// Add other methods here:
-
+	/* 2. Count how many oysters contain pearls. */
 	public static int countPearls(List<Boolean> oysters) {
 		int Pearls = 0;
 		for (int b = 0; b < oysters.size(); b++) {
@@ -26,21 +27,64 @@ public class Algorithms {
 		}
 		return Pearls;
 	}
-
+	
+	/* 3. Create a method that will return the height of the tallest person. */
 	public static double findTallest(List<Double> peeps) {
-			double p = 0;
-		 for (int c = 0; c < peeps.size(); c++) {
-			 if (peeps.get(c) > p) {
-				p = peeps.get(c);
-			} 
-			p = peeps.get(c);
+		double d = 0;
+		for (int c = 0; c < peeps.size(); c++) {
+			if (peeps.get(c) > d) {
+				d = peeps.get(c);
+			}
+			//d = peeps.get(c);
 		}
-		 return p;
-	 }
-	public static boolean containsSOS(List<String> message) {
-		if(message.contains("... --- ...")) {
-			return true;
-		}
-
+		return d;
 	}
+	
+	/* 4. Create a method that will find the longest word. */
+	public static String findLongestWord(List<String> words) {
+		String f = "";
+		for (int e = 0; e < words.size(); e++) {
+			if (words.get(e).length() > f.length()) {
+				f = words.get(e);
+			}
+			//f = words.get(e);
+		}
+		return f;
+	}
+	
+	/*
+	 * 5. The Morse code for SOS is "... --- ..."
+	 * Create a method that can tell if SOS is contained in a message.
+	 */
+ public static boolean containsSOS(List<String> message) {
+	 for(int g = 0; g < message.size(); g++) {
+ if(message.get(g).contains("... --- ...")) {
+ return true;
+ }
+	 }
+	 return false;
+ }
+ 
+ /* 6. Write a morse code translator to read the messages. Just kidding! */ //√
+ 
+ /* 7. Sort the exam results from lowest to highest. */
+ public static List<Double> sortScores(List<Double> results) {
+	
+	 for(int h = 0; h < results.size(); h++) {
+		 for(int i = h+1; i < results.size(); h++)
+		 if(results.get(h) > results.get(i)) {
+			 results.set(h, results.get(i));
+		 }
+		 else {}
+	 }
+	 return results;
+ }
+	/* 8. Sort the DNA sequences by length from shortest to longest. */
+ 
+  public static String sortDNA(List<String> unsortedDNA) {
+	  
+	  
+  }
+ 
+ 
 }
